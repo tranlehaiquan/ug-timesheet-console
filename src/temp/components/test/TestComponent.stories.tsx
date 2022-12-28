08bb8902b7137bd3411db44829ce686b0cf81434
quantran020173 <tranlehaiquan@gmail.com>
2022-12-28 15:01:16 +0700

    remove test and stories

--- src/temp/components/test/TestComponent.stories.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-
-import { TestComponent } from "./TestComponent";
-
-if (process.env.NODE_ENV === "development") {
-  storiesOf("Test", module).add("Overview", () => <TestComponent />);
-}
--- src/temp/components/test/TestComponent.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export const TestComponent = () => (
-  <div className="sk-bg-grey sk-text-white">Test Component</div>
-);
