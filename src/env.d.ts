7ec9ae9f49fc8f61c80679bc1be554f0ce884698
quantran020173 <tranlehaiquan@gmail.com>
2022-12-29 11:34:26 +0700

    bugfix build

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,3 +8,10 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+import * as React from '@types/react';
+declare module 'react' {
+  interface FunctionComponent<P = {}> {
+    (props: PropsWithChildren<P>, context?: any): ReactElement<any, any> | null;
+  }
+}
